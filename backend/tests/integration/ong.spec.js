be44086dd4a6bbaaa5c43a7a5fb10d7401b0dfa7
TI-1986 <tiagohanrysites@gmail.com>
2020-03-29 10:12:24 -0300

    iniciando testes

--- backend/tests/integration/ong.spec.js
+++ backend/tests/integration/ong.spec.js
@@ -1,8 +1,23 @@
-const request = require('supertest');
+const req = require('supertest');
+const app = require('../../src/app');
+const connection = require('../../src/database/connection');
 
 describe('ONG', () => {
-    it ('shoud be able to create a new ONG', () => {
-        
+    beforeEach( async () => {
+       await connection.migrate.rollback();
+       await connection.migrate.latest();
 
     });
-});
+    it ('shoud be able to create a new ONG', async () => {
+        const res = await req(app).post('/ongs').send({
+            name:"Lar colatina",
+	        email:"contato@lish.com.br",
+	        whatsapp:"2799777777",
+	        city:"Colatina",
+	        uf:"ES"
+        });
+
+       expect(res.body).toHaveProperty('id');
+       expect(res.body.id).toHaveLength(8);
+    });
+})
